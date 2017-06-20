e4137c2d9f9fc4d1b7dc0bb02e88d8ab60005de1
YAHIA-HADDADI <yahia.yahia.ha@gmail.com>
2017-06-20 05:48:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ client.on('message', message => {
 if (message.content === 'ping') {
 message.reply('pong');
 }
-   else if (message.content.search("!blague") != -1) {
+   else if (message.content === '!blague') {
 			
 			axios.get('http://www.chucknorrisfacts.fr/api/get?data=tri:alea;nb=1').then(function(response){
 				message.reply(response.data[0].fact);
